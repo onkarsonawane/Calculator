739c2b7374ec2d4f0d382e16a1aa8148565c8769
onkarsonawane <141544119+onkarsonawane@users.noreply.github.com>
2025-02-07 11:12:59 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+// src/app/app.component.ts
+
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
+})
+export class AppComponent {
+    title = 'calculator';
+    display: string = '';
+
+    appendInput(value: string): void {
+        this.display += value;
+    }
+
+    clear(): void {
+        this.display = '';
+    }
+
+    calculate(): void {
+        try {
+            this.display = eval(this.display);
+        } catch (e) {
+            this.display = 'Error';
+        }
+    }
+}
